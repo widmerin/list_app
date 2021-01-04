434a260a141ac53064b72b0a62b24b02faf89e8b
Ina Widmer <ina.widmer@unic.com>
2021-01-04 17:17:52 +0100

    Setup Todo List

---
+++ src/components/TodoList.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What do you need?" v-model="newTodo" @keyup.enter="addTodo"/>
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+        {{ todo.title }}
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      newToDo: '',
+      idForTodo: 4,
+      todos: [
+        {
+          'id': 1,
+          'title': 'Task 1',
+          'completed': false
+        },
+        {
+          'id': 2,
+          'title': 'Task 2',
+          'completed': false
+        },
+        {
+          'id': 3,
+          'title': 'Task 3',
+          'completed': true
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+      this.newTodo = ''
+      this.idForTodo++
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    animation-duration: 0.3s;
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
+    }
+  }
+  .todo-item-left { // later
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc; //override defaults
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    &:focus {
+      outline: none;
+    }
+  }
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+    &:hover {
+      background: lightgreen;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  .active {
+    background: lightgreen;
+  }
+  // CSS Transitions
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,41 @@
 <template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+  <div id="app" class="container">
+    <img src="../assets/logo.svg" class="logo">
+    <todo-list></todo-list>
+  </div>
 </template>
-
 <script>
+import TodoList from '~/components/TodoList.vue'
+
 export default {
+    components: {
+    TodoList
+  },
   metaInfo: {
     title: 'Hello, world!'
   }
 }
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style lang='scss'>
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
 </style>
