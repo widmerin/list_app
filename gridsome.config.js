d5bd8ba09815ae9855b0fec3b047ef4611f08f32
Ina Widmer <ina.widmer@unic.com>
2021-01-17 12:22:36 +0100

    Enable PWA plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,27 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Shopping List',
-  plugins: []
-}
+  siteName: "Gridsome",
+  plugins: [
+    {
+      use: "gridsome-plugin-pwa",
+      options: {
+        title: "The List",
+        startUrl: "/",
+        display: "standalone",
+        statusBarStyle: "default",
+        manifestPath: "manifest.json",
+        disableServiceWorker: true,
+        serviceWorkerPath: "service-worker.js",
+        cachedFileTypes: "js,json,css,html,png,jpg,jpeg,svg",
+        shortName: "List",
+        themeColor: "#666600",
+        backgroundColor: "#ffffff",
+        icon: "src/favicon.png", // must be provided like 'src/favicon.png'
+        msTileImage: "",
+        msTileColor: "#666600",
+        gcmSenderId: undefined,
+      },
+    },
+  ],
+};
