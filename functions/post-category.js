e35a7b7b0bbbe9662e915889ba9095cd7879fbba
Ina Widmer <ina.widmer@unic.com>
2021-01-29 14:03:17 +0100

    Add post category function

---
+++ functions/post-category.js
@@ -0,0 +1,25 @@
+const faunadb = require('faunadb');
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+})
+
+exports.handler = (event, context, callback) => {
+  const data = JSON.parse(event.body)
+  const newCategory = {
+    data: {
+      name: "Category XY"
+    }
+  }
+  return client.query(q.Create(q.Ref("collections/categories"), newCategory))
+  .then((response) => {
+    console.log("success", response)
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    })
+  }).catch((error) => {
+    console.log(error)
+  })
+}
